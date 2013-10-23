1e8ec1a552a960f6834f0598366133f6e4a329f3
Sherief Gharraph <sherief@Sheriefs-MacBook-Pro.local>
2013-10-22 21:50:10 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourspedia::Application.config.secret_token = '27fb86cdb75665b0cef2d9ff6064f5ec9b71b21ef399d8a506d41ab5c0f7d55e499e6bae22319fd6307630c9d6fd0e385ced76f10862cae7ea722bb87c45574c'
